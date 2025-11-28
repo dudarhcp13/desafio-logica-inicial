3434f02cf93dc79fd96add640201d5031265775b
dudarhcp13 <mefajardofly@gmail.com>
2025-11-28 20:13:52 -0300

    final

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("Olá mundo!")
+let nomeDoHeroi = "Morgana"
+let xp = 10000
+
+
+if(xp < 1000){
+    console.log("O nível do seu Herói + " + nomeDoHeroi + "está no nível Ferro")
+}else if(xp >= 1001 && xp <= 2000){
+    console.log("O nível do seu Herói + " + nomeDoHeroi + "está no nível Bronze")
+}else if(xp (2001 <= 5000)){
+    console.log("O nível do seu Herói + " + nomeDoHeroi + "está no nível Prata")
+}else if(xp(5001 <= 7000)){
+    console.log("O nível do seu Herói é: Ouro")
+}else if(xp(7001 <= 8000)){
+    console.log("O nível do seu Herói é: Platina")
+}else if(xp(8001 <= 9000)){
+    console.log("O nível do seu Herói é: Ascendente")
+}else if(xp(9001 <= 10000)){
+    console.log("O nível do seu Herói é: Imortal")
+}else xp >= 10001;{
+    console.log("O nível do seu Herói é: Radiante")
+}
+
+console.log("O Herói de nome:" + minhaHeroina + "está no nível: ")
