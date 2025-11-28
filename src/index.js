aac10229a718c027a09f90f60fb31e085ff6f58e
dudarhcp13 <mefajardofly@gmail.com>
2025-11-28 20:20:38 -0300

    final

--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@ let xp = 10000
 
 
 if(xp < 1000){
-    console.log("O nível do seu Herói + " + nomeDoHeroi + "está no nível Ferro")
+    console.log("O nível do seu Herói + " + nomeDoHeroi + "é o nível Ferro")
 }else if(xp >= 1001 && xp <= 2000){
-    console.log("O nível do seu Herói + " + nomeDoHeroi + "está no nível Bronze")
+    console.log("O nível do seu Herói + " + nomeDoHeroi + "é o nível Bronze")
 }else if(xp >= 2001 && xp <= 5000){
-    console.log("O nível do seu Herói + " + nomeDoHeroi + "está no nível Prata")
+    console.log("O nível do seu Herói + " + nomeDoHeroi + "é o nível Prata")
 }else if(xp(5001 <= 7000)){
-    console.log("O nível do seu Herói + " + nomeDoHeroi + "está no nível Ouro")
+    console.log("O nível do seu Herói + " + nomeDoHeroi + "é o nível Ouro")
 }else if(xp >= 7001 && xp <= 8000){
-    console.log("O nível do seu Herói + " + nomeDoHeroi + "está no nível Platina")
+    console.log("O nível do seu Herói + " + nomeDoHeroi + "é o nível Platina")
 }else if(xp >= 8001 && xp <= 9000){
-    console.log("O nível do seu Herói + " + nomeDoHeroi + "está no nível Ascendente")
+    console.log("O nível do seu Herói + " + nomeDoHeroi + "é o nível Ascendente")
 }else if(xp >= 9001 && XP <= 10000){
-    console.log("O nível do seu Herói + " + nomeDoHeroi + "está no nível Imortal")
+    console.log("O nível do seu Herói + " + nomeDoHeroi + "é o nível Imortal")
 }else xp >= 10001;{
-    console.log("O nível do seu Herói + " + nomeDoHeroi + "está no nível Radiante")
+    console.log("O nível do seu Herói + " + nomeDoHeroi + "é o nível Radiante")
 }
 
