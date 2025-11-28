f99b930ca9becf6d251c6f3a8f248b0c99922279
dudarhcp13 <mefajardofly@gmail.com>
2025-11-28 20:35:30 -0300

    final

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 let nomeDoHeroi = "Morgana"
 let xp = 10000
-
+let nivelHeroi
 
 if(xp < 1000){
-    console.log("O nível do seu Herói + " + nomeDoHeroi + "é o nível Ferro")
+    nivelHeroi = "Ferro"
 }else if(xp >= 1001 && xp <= 2000){
-    console.log("O nível do seu Herói + " + nomeDoHeroi + "é o nível Bronze")
+    nivelHeroi = "Bronze"
 }else if(xp >= 2001 && xp <= 5000){
-    console.log("O nível do seu Herói + " + nomeDoHeroi + "é o nível Prata")
-}else if(xp(5001 <= 7000)){
-    console.log("O nível do seu Herói + " + nomeDoHeroi + "é o nível Ouro")
+    nivelHeroi = "Prata"
+}else if(xp >= 5001 && xp <= 7000){
+    nivelHeroi = "Ouro"
 }else if(xp >= 7001 && xp <= 8000){
-    console.log("O nível do seu Herói + " + nomeDoHeroi + "é o nível Platina")
+    nivelHeroi = "Platina"
 }else if(xp >= 8001 && xp <= 9000){
-    console.log("O nível do seu Herói + " + nomeDoHeroi + "é o nível Ascendente")
+    nivelHeroi = "Ascendente"
 }else if(xp >= 9001 && XP <= 10000){
-    console.log("O nível do seu Herói + " + nomeDoHeroi + "é o nível Imortal")
+    nivelHeroi = "Imortal"
 }else xp >= 10001;{
-    console.log("O nível do seu Herói + " + nomeDoHeroi + "é o nível Radiante")
+    nivelHeroi = "Radiante"
 }
 
+console.log("O nível do seu Herói " + nomeDoHeroi + "é o nível " + nivelHeroi)
